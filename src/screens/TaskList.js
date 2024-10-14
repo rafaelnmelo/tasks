eaa01781ea4437fccabd337845b87c40ff1553f0
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-10-14 15:17:11 -0300

    Iniciando primeira tela do app e criando estilos

---
+++ src/screens/TaskList.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react"
+import {
+    View, Text, ImageBackground,
+    StyleSheet
+} from "react-native"
+
+import todayImage from '../../assets/imgs/today.jpg'
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg'
+import weekImage from '../../assets/imgs/week.jpg'
+import monthImage from '../../assets/imgs/month.jpg'
+
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
+export default class TaskList extends Component {
+    render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage}
+                    style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>
+                            Hoje
+                        </Text>
+                        <Text style={styles.subtitle}>
+                            {today}
+                        </Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Text>Tarefa #01</Text>
+                    <Text>Tarefa #02</Text>
+                    <Text>Tarefa #03</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30
+    },
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'space-between',
+        marginTop: Platform.OS === 'ios' ? 40 : 10
+    },
+    addButton: {
+        position: 'absolute',
+        right: 30,
+        bottom: 30,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
