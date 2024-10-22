8105161218ecabbef33e456709b8808e3a460098
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-10-22 11:46:02 -0300

    Instalando react-native-reanimated

--- ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
+++ ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
@@ -41,6 +41,8 @@
 #import "rnasyncstorageJSI.h"
 #import "rngesturehandler_codegen/rngesturehandler_codegen.h"
 #import "rngesturehandler_codegenJSI.h"
+#import "rnreanimated/rnreanimated.h"
+#import "rnreanimatedJSI.h"
 #import "rnscreens/rnscreens.h"
 #import "rnscreensJSI.h"
 #import "RNVectorIconsSpec/RNVectorIconsSpec.h"
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,10 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// metro.config.js
+const {
+    wrapWithReanimatedMetroConfig,
+  } = require('react-native-reanimated/metro-config')
+  
+  const config = {
+    // Your existing Metro configuration options
+  }
+  
+  module.exports = wrapWithReanimatedMetroConfig(config)
