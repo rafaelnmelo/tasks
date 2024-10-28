08c160056924bc550197a0b4fda3f0ea64a095ee
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-10-28 13:05:11 -0300

    Criando navegação

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native'
 import {name as appName} from './app.json'
-import TaskList from './src/screens/TaskList'
+import Navigator from './src/Navigator'
 
-AppRegistry.registerComponent(appName, () => TaskList)
+AppRegistry.registerComponent(appName, () => Navigator)
