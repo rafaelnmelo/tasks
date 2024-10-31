d26a6913e7a6c5f5c6e4594d8a0de256e4f25e0a
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-10-31 12:06:39 -0300

    Criando cadastro integrado com o backend

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -4,9 +4,12 @@ import {
     View, TouchableOpacity, Alert
 } from "react-native"
 
+import axios from 'axios'
+
 import backgroundImg from '../../assets/imgs/login.jpg'
 import commonStyles from "../commonStyles"
 import AuthInput from "../components/AuthInput"
+import { server, showError, showSuccess } from "../common"
 
 export default class Auth extends Component {
 
@@ -19,7 +22,22 @@ export default class Auth extends Component {
     }
 
     signinOrSignup = () => {
-        this.state.stageNew ? Alert.alert('Sucesso', 'Criar conta') : Alert.alert('Sucesso', 'Logar')
+        this.state.stageNew ? this.signup() : Alert.alert('Sucesso', 'Logar')
+    }
+
+    signup = async () => {
+        try {
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword
+            })
+            showSuccess('Usuário cadastrado!')
+            this.setState({ stageNew: false })
+        } catch (error) {
+            showError(error)
+        }
     }
 
     render() {
