c812cdbf4442612f8cf792e519e6f183c88f130a
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-11-04 13:23:15 -0300

    Implementação navegação por Drawer

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,23 +1,49 @@
-import {
-    createAppContainer, createSwitchNavigator
-} from 'react-navigation'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
-import Auth from "./screens/Auth"
-import TaskList from "./screens/TaskList"
+import Auth from './screens/Auth'
+import TaskList from './screens/TaskList'
 
-const mainRoutes = {
-    Auth: {
-        name: 'Auth',
-        screen: Auth
-    },
-    Home: {
-        name: 'Home',
-        screen: TaskList
-    }
+const Stack = createStackNavigator()
+const Drawer = createDrawerNavigator()
+
+const DrawerNavigator = props => {
+    // const { email, name } = props.route.params
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="Today" options={{ title: 'Hoje' }}>
+                {props => <TaskList {...props} title='Hoje' daysAhead={0} />}
+            </Drawer.Screen>
+            <Drawer.Screen name="Tomorrow" options={{ title: 'Amanhã' }}>
+                {props => <TaskList {...props} title='Amanhã' daysAhead={1} />}
+            </Drawer.Screen>
+            <Drawer.Screen name="Week" options={{ title: 'Semana' }}>
+                {props => <TaskList {...props} title='Semana' daysAhead={7} />}
+            </Drawer.Screen>
+            <Drawer.Screen name="Month" options={{ title: 'Mês' }}>
+                {props => <TaskList {...props} title='Mês' daysAhead={30} />}
+            </Drawer.Screen>
+        </Drawer.Navigator>
+    )
 }
 
-const mainNavigator = createSwitchNavigator(mainRoutes, {
-    initialRouteName: 'Auth'
-})
+const AuthNavigator = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Auth" component={Auth} />
+            <Stack.Screen name="Home" component={DrawerNavigator} />
+        </Stack.Navigator>
+    )
+}
 
-export default createAppContainer(mainNavigator)
+const Navigator = () => {
+    return (
+        <NavigationContainer>
+            <AuthNavigator />
+        </NavigationContainer>
+    )
+}
+
+export default Navigator
