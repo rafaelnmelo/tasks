53c260d9453027d351be3088a25a17a7c92f40e9
Rafael Nunes <rafaelnunes@iMac-Pro-de-Rafael.local>
2024-11-05 10:25:38 -0300

    Melhorando mensagem de error ao entrar no app

--- src/common.js
+++ src/common.js
@@ -7,7 +7,11 @@ const server = Platform.OS === 'ios'
     ? 'http://localhost:3000' : 'http://10.0.2.2:3000'
 
 function showError(error) {
-    Alert.alert('Ops! ocorreu um problema', `Mensagem: ${error}`)
+    if (error.response && error.response.data) {
+        Alert.alert('Ops! ocorreu um problema', `Mensagem: ${error.response.data}`)
+    } else {
+        Alert.alert('Ops! ocorreu um problema', `Mensagem: ${error}`)
+    }
 }
 
 function showSuccess(message) {
